feb5a045df44320883fd77c3826da1310a012277
Adebunmi <dfnsbetsy@gmail.com>
2026-06-10 23:01:24 -0400

    Add all project files

---
+++ app/main.py
@@ -0,0 +1,18 @@
+# Just addint a comment.
+import os
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+PREFIX = os.environ.get("GREETING_PREFIX", "Hello")
+
+@app.get("/health")
+def health():
+    return jsonify(status="ok")
+
+@app.get("/greet")
+def greet():
+    name = request.args.get("name", "World")
+    return jsonify(message=f"{PREFIX}, {name}!")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
